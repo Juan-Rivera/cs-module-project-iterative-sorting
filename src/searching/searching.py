b6e3d27245ce48f3fbeda3cfbada93f630b9bb77
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-08-18 13:16:58 -0400

    linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,12 @@
 def linear_search(arr, target):
     # Your code here
+    for index in range(len(arr)):
+        if arr[index] == target:
+            return index
 
+    # for thing in arr:
+    #     if thing == target:
+    #         return arr.index(target)
 
     return -1   # not found
 
@@ -9,6 +15,5 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
+    
     return -1  # not found
